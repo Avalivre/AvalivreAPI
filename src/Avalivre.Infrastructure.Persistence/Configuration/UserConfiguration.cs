74a3b97911f6250752a57df8453afc4b55083d07
Vitor Filardi <rabelo51@gmail.com>
2020-11-02 19:33:36 -0300

    Create UserConfiguration for sqlserver

---
+++ src/Avalivre.Infrastructure.Persistence/Configuration/UserConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using Avalivre.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Avalivre.Infrastructure.Persistence.Configuration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder
+                .ToTable("Users")
+                .HasKey(u => u.Id);
+
+            builder
+                .Property(u => u.Id)
+                .HasColumnName("USR_Id")
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+
+            builder
+                .Property(u => u.Name)
+                .HasColumnName("USR_Name")
+                .HasMaxLength(256)
+                .IsRequired();
+
+            builder
+                .Property(u => u.Email)
+                .HasColumnName("USR_Email")
+                .HasMaxLength(256)
+                .IsRequired();
+
+            builder
+                .Property(u => u.Password)
+                .HasColumnName("USR_Password")
+                .HasMaxLength(256)
+                .IsRequired();
+
+            builder
+                .Property(u => u.IsAdmin)
+                .HasColumnName("USR_IsAdmin")
+                .IsRequired();
+        }
+    }
+}
