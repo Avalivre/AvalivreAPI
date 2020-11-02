4dd1b5ff84de4d0edf4c7b78f5bca0b9d4d817d8
Vitor Filardi <rabelo51@gmail.com>
2020-11-02 20:16:09 -0300

    Add private set again: it's necessary for entity framework

--- src/Avalivre.Domain/Users/User.cs
+++ src/Avalivre.Domain/Users/User.cs
@@ -10,11 +10,11 @@
             this.IsAdmin = isAdmin;
         }
 
-        public int Id { get; }
+        public int Id { get; private set; }
         public string Name { get; private set; }
         public string Email { get; private set; }
         public string Password { get; private set; }
-        public bool IsAdmin { get; }
+        public bool IsAdmin { get; private set; }
 
         public void SetName(string name)
         {
