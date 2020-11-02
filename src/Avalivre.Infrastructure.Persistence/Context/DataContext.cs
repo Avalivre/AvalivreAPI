938b9762d3ea50c72aa35d726081d863d2cc7b14
Vitor Filardi <rabelo51@gmail.com>
2020-11-02 19:29:54 -0300

    Create DataContext

---
+++ src/Avalivre.Infrastructure.Persistence/Context/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Avalivre.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace Avalivre.Infrastructure.Persistence.Context
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
