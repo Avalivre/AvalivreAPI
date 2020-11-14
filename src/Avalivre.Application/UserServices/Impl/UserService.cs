4014c16c5b01cda35cd57b7de5b35febf94baf2f
Vitor Filardi <rabelo51@gmail.com>
2020-11-14 19:33:44 -0300

    Create UserService Impl

---
+++ src/Avalivre.Application/UserServices/Impl/UserService.cs
@@ -0,0 +1,24 @@
+﻿using Avalivre.Domain.Users;
+using Avalivre.Infrastructure.DTO.UserAuth;
+using System.Threading.Tasks;
+
+namespace Avalivre.Application.UserServices.Impl
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            this._userRepository = userRepository;
+        }
+        public async Task Register(RegisterUserDTO dto)
+        {
+            var user = new User(dto.Name, dto.Email, dto.Password);
+
+            _userRepository.Insert(user);
+
+            await _uow.SaveChangesAsync();
+        }
+    }
+}
