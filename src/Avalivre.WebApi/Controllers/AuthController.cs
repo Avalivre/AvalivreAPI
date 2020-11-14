24b125778aba46023f891d2b328cfa3154797cd0
Vitor Filardi <rabelo51@gmail.com>
2020-11-14 18:12:00 -0300

    Add AuthController + auth/register endpoint

---
+++ src/Avalivre.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Avalivre.WebApi.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> RegisterUser(
+            [FromServices] IUserService userService,
+            [FromBody] RegisterUserDTO dto)
+        {
+            try
+            {
+                userService.RegisterUser(dto);
+
+                return CreatedAtAction("register", dto.Name);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
