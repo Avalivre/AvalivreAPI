2aa03abc32de4c82132b0bd68a3ce847c4f188d9
Vitor Filardi <rabelo51@gmail.com>
2020-11-16 21:04:28 -0300

    Create Login DTOs

---
+++ src/Avalivre.Infrastructure.DTO/UserAuth/LoginUserResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Avalivre.Infrastructure.DTO.UserAuth
+{
+    public class LoginUserResponseDTO
+    {
+        public string Message { get; set; }
+        public string Token { get; set; }
+    }
+}
