ad854c804885b5a450a8235552e156e25d7b28c7
Vitor Filardi <rabelo51@gmail.com>
2020-11-18 20:10:59 -0300

    Create api/product/ post

---
+++ src/Avalivre.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Avalivre.WebApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> Create(
+            [FromServices] IProductService productService,
+            [FromBody] CreateProductDTO dto)
+        {
+            try
+            {
+                var response = productService.Create(dto);
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500);
+            }
+        }
+    }
+}
