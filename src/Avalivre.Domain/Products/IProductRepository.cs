9dfa910268900211b42d11c8b6380ef0997f2a2e
Vitor Filardi <rabelo51@gmail.com>
2020-11-20 18:45:52 -0300

    Create repository method GetSimilarProducts

--- src/Avalivre.Domain/Products/IProductRepository.cs
+++ src/Avalivre.Domain/Products/IProductRepository.cs
@@ -1,7 +1,12 @@
-﻿namespace Avalivre.Domain.Products
+﻿using Avalivre.Infrastructure.DTO.Product;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Avalivre.Domain.Products
 {
     public interface IProductRepository
     {
         void Insert(Product product);
+        Task<IEnumerable<SimilarProductDTO>> GetSimilarProducts(string name, int fetch);
     }
 }
